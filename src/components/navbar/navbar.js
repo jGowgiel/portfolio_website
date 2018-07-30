69fc21a414633b7c3b564bd68da67365259779a7
Jarod Gowgiel <jarod.gowgiel@tufts.edu>
2018-07-29 23:16:20 -0700

    Repositioning the collapse toggler

--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -10,10 +10,11 @@ class Navbar extends React.Component {
     render () {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                <Link to='/' className="navbar-brand">Jarod Gowgiel</Link>
+                
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
+                <Link to='/' className="navbar-brand">Jarod Gowgiel</Link>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         <li className="nav-item">
