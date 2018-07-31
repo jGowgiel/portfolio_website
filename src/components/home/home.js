469b0bcb93782d3408b791886fdf86016e28c9ed
Jarod Gowgiel <jarod.gowgiel@tufts.edu>
2018-07-30 20:04:38 -0700

    Side by side image and home page text

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -21,24 +21,21 @@ class Home extends React.Component {
         return (
             <div className='around-nav'>
                 <img src="images/OverwatchBanner.jpg" className="img-fluid" alt="Overwatch Image" />
+                <div className="row">
+                    <div className="container-fluid py-3 m-3 text-center col-8">
+                        <div className="h4 py-3">Hi there!</div>
+                        <div className="h4 font-weight-normal">
+                            <p>My name's Jarod - I'm an aspiring software developer and a current computer science student at Tufts University with a strong background in fabrication and 3D design.</p>
+                            <p>The bulk of my programming experience is in C/C++, Swift, and Javascript.</p>
+                            <p>Please, feel free to explore around using the links at the top, and get in touch through the contact section if you're interested in talking with me about my work, my projects, or my job experience</p>
+                        </div>
 
-                <div className="container-fluid py-3 text-center">
-                    <div className="h4 py-3">Hi there!</div>
-                    <div className="h4 font-weight-normal">
-                        <p>My name's Jarod - I'm an aspiring software developer and a current computer science student at Tufts University with a strong background in fabrication and 3D design.</p>
-                        <p>The bulk of my programming experience is in C/C++, Swift, and Javascript.</p>
-                        <p>Please, feel free to explore around using the links at the top, and get in touch through the contact section if you're interested in talking with me about my work, my projects, or my job experience</p>
                     </div>
 
-                </div>
-
-                <div className="container-fluid">
-                    <div className="row">
-                        <div className="col-5"></div>
-                        <div className="col-2">
+                    <div className="container-fluid col py-5 px-3">
+                        <div className="py-5 px-3">
                             <img src="images/jarod_g.jpg" className="img-fluid rounded" alt="Self Image" />
                         </div>
-                        <div className="col-5"></div>
                     </div>
                 </div>
             </div>
--- src/components/navbar/navbar.js
+++ src/components/navbar/navbar.js
@@ -10,7 +10,6 @@ class Navbar extends React.Component {
     render () {
         return (
             <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-                
                 <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
